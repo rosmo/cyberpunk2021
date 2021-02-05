01a76dc8acfb075780c8c1e03b5c601be76cb412
Nikolai Danylchyk <n.danylchyk@shoop.de>
2021-02-05 20:52:52 +0100

    Updated build script publish.sh

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-minify app/assets/js/main.js > app/assets/js/main.min.js
-minify app/assets/css/main.css > app/assets/css/main.min.css
+minify app/assets/js/main.dev.js > app/assets/js/main.min.js
+minify app/assets/css/main.dev.css > app/assets/css/main.min.css
+sed -i -e 's/main.dev/main.min/g' app/index.html
 wrangler publish
+sed -i -e 's/main.min/main.dev/g' app/index.html
